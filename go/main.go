e912539a5ae0e238e1076ca5732f05bc6eeccaf0
amsen <ahph1380@gmail.com>
2024-05-22 15:23:49 -0400

    Add go main

---
+++ go/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"time"
+	"webcrawler/crawler"
+)
+
+func main() {
+	timeout := flag.Int("timeout", 0, "duration of crawling")
+	maxConnections := flag.Int("max-connections", 0, "maximum number of parallel connections")
+	flag.Parse()
+
+	if timeout == nil || maxConnections == nil {
+		panic("timeout and maxConnections are required")
+	}
+	wc := crawler.New()
+	duration, err := time.ParseDuration(fmt.Sprintf("%dms", *timeout))
+	if err != nil {
+		panic(err)
+	}
+
+	crawler.RunExperiment(wc, duration, *maxConnections)
+}
